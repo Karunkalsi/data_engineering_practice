d9bafff07dc3713ed44ecf822f40e3a37d840ef1
Karunkalsi <karunjit.kalsi@ucalgary.ca>
2023-05-23 11:08:47 -0600

    general formatting cleanup

--- stock__ml_project/data_input.py
+++ stock__ml_project/data_input.py
@@ -2,23 +2,27 @@ import yfinance as yf
 import yahoo_fin.stock_info as si
 import pandas as pd
 
-def input_data(number_of_tickers):
+
+def input_data(number_of_tickers: int, index: str):
     """
     This function takes in a select number of tickers and returns a dataframe
     with that amount of top producers
     :param number_of_tickers: int of top producers
-    :return: top producers: dataframe
+    :return: top and worst producers: dataframe
     """
+    index_dict = {
+        "dow": si.tickers_dow(),
+        "s&p": si.tickers_sp500(),
+        "nasdaq": si.tickers_nasdaq()
+    }
 
-    # top_producers = si.tickers_sp500()
-    # top_producers = si.tickers_dow()
-    top_producers = si.tickers_nasdaq()
+    all_producers = index_dict[index]
 
     # Create an empty dictionary to store the daily percentage changes of the companies
     daily_pct_change_dict = {}
 
     # Iterate over the companies and retrieve their daily percentage change
-    for company in top_producers:
+    for company in all_producers:
         try:
             data = si.get_data(company)
             data['Daily_Pct_Change'] = data['close'].pct_change() * 100
@@ -31,22 +35,13 @@ def input_data(number_of_tickers):
     sorted_pct_changes = sorted(daily_pct_change_dict.items(), key=lambda x: x[1], reverse=True)
 
     # Get the top 10 best performing stocks
-    top_10_best = sorted_pct_changes[:10]
+    top_10_best = sorted_pct_changes[:number_of_tickers]
 
     # Get the top 10 worst performing stocks
-    top_10_worst = sorted_pct_changes[-10:]
+    top_10_worst = sorted_pct_changes[-number_of_tickers:]
 
-    # Print the ticker symbols and daily percentage changes of the top 10 best performing stocks
-    print("Top 10 Best Performing Stocks:")
-    for stock in top_10_best:
-        print(f"Ticker: {stock[0]}, Daily Pct Change: {stock[1]}")
+    return top_10_best, top_10_worst
 
-    # Print the ticker symbols and daily percentage changes of the top 10 worst performing stocks
-    print("\nTop 10 Worst Performing Stocks:")
-    for stock in top_10_worst:
-        print(f"Ticker: {stock[0]}, Daily Pct Change: {stock[1]}")
-
-    return top_10_best
 
 if __name__ == "__main__":
     print(input_data(10))
--- stock__ml_project/main.py
+++ stock__ml_project/main.py
@@ -2,7 +2,28 @@ from data_input import input_data
 import pandas as pd
 
 def run():
-    prev_data = input_data()
+    # Choose which index to get the data from
+    # dow, s&p, or nasdaq
+    index = 'dow'
+
+    # Choose how many tickers to compare
+    number_of_tickers = 5
+
+    best, worst = input_data(number_of_tickers=number_of_tickers, index=index)
+    print_tickers(best, worst, number_of_tickers)
+
+
+def print_tickers(best, worst, number_of_tickers=10):
+    # Print the ticker symbols and daily percentage changes of the top 10 best performing stocks
+    print(f"Top {number_of_tickers} Best Performing Stocks:")
+    for stock in best:
+        print(f"Ticker: {stock[0]}, Daily Pct Change: {stock[1]}")
+
+    # Print the ticker symbols and daily percentage changes of the top 10 worst performing stocks
+    print(f"\nTop {number_of_tickers} Worst Performing Stocks:")
+    for stock in worst:
+        print(f"Ticker: {stock[0]}, Daily Pct Change: {stock[1]}")
+
 
 if __name__ == "__main__":
     run()
